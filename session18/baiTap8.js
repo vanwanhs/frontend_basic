7c3be6291e062693d69b759da61814760f6939b0
vanwanhs <hivanwnguyen@gmail.com>
2025-03-01 14:51:20 +0700

    bai tap session 18

--- session18/baiTap8.js
+++ session18/baiTap8.js
@@ -1,24 +1,4 @@
-const a = parseFloat(prompt("Nhập cạnh a: "));
-const b = parseFloat(prompt("Nhập cạnh b: "));
-const c = parseFloat(prompt("Nhập cạnh c: "));
-
-let result;
-if (a + b > c && a + c > b && b + c > a) {
-    if (a === b && b === c) {
-        result = "Tam giác đều";
-    } else if (a === b || a === c || b === c) {
-        result = "Tam giác cân";
-    } else if (
-        a * a + b * b === c * c ||
-        a * a + c * c === b * b ||
-        b * b + c * c === a * a
-    ) {
-        result = "Tam giác vuông";
-    } else {
-        result = "Tam giác thường";
-    }
-} else {
-    result = "Không phải tam giác hợp lệ";
-}
-
-alert(result);
+let a=Number(prompt("Mời bạn nhập số a"));
+let b=Number( prompt("Mời bạn nhập số b"));
+let comparator(prompt("Mời bạn nhập phép tính "));
+if (comparator)
