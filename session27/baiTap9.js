72b9ac27f301192691f50f63e4e85e835fad51a8
vanwanhs <hivanwnguyen@gmail.com>
2025-03-13 22:04:26 +0700

    bai tap session 27

---
+++ session27/baiTap9.js
@@ -0,0 +1,14 @@
+function sortEvenNumbers(arr) {
+    if (!Array.isArray(arr) || arr.some(isNaN)) {
+        console.log("Dữ liệu không hợp lệ");
+    }
+    
+    let evenNumbers = arr.filter(num => num % 2 === 0).sort((a, b) => a - b);
+    let evenIndex = 0;
+    
+    return arr.map(num => num % 2 === 0 ? evenNumbers[evenIndex++] : num);
+}
+
+console.log(sortEvenNumbers([5, 3, 2, 8, 1, 4]));
+console.log(sortEvenNumbers([9, 4, 6, 7, 2, 3])); 
+console.log(sortEvenNumbers("abc"));
